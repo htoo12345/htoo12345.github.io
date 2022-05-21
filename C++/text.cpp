3ea67555749e2bb4a2f0e476f2aae490cb8e53a0
htoo12345 <htooaunglynn6@gmail.com>
2022-05-21 19:29:17 +0630

    add assignment

---
+++ C++/text.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World \n";
+    
+    Add();
+
+    cout<<"\nEnter First Number  ";
+
+}
+
+int Add()
+{
+    int firstNum = 100;
+    int secondNum = 390;
+    int result = firstNum + secondNum;
+    cout<<result ;
+
+    return 0;
+}
