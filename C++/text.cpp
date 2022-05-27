ac028a30c2d0d58f3f18c7412f7585d6c3e1524b
htoo12345 <htooaunglynn6@gmail.com>
2022-05-27 20:51:19 +0630

    add cpp and assignment

--- C++/text.cpp
+++ C++/text.cpp
@@ -5,18 +5,9 @@ int main()
 {
     cout<<"Hello World \n";
     
-    Add();
+    // Add();
 
     cout<<"\nEnter First Number  ";
 
 }
 
-int Add()
-{
-    int firstNum = 100;
-    int secondNum = 390;
-    int result = firstNum + secondNum;
-    cout<<result ;
-
-    return 0;
-}
