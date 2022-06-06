0ae0054635f48705d9327779e75fb36a38bee2d6
htoo12345 <htooaunglynn6@gmail.com>
2022-06-06 23:05:09 +0630

    add assignment

--- cpp/Assignment/sun_input_no_assignment.cpp
+++ cpp/Assignment/sun_input_no_assignment.cpp
@@ -1,31 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int no[20];
-    int j ,sum = 0, c;
-    
-    cout << "How many number : ";
-    cin >> c;
+    //variable declaration
+    int n, n1, num = 0,m;
 
-    cout << "Enter number : " << endl;
-    for (int i = 0; i < c; i++)
-    {
-        cin >> j;
-        no[i] =j;
-        sum += j;
-    }
-    
-    cout << "The sum of digit of " ;
+    //taking input from the command line (user)
+    cout << " Enter a positive integer :  ";
+    cin >> n; //12345 // 111 
 
-    for (int i = 0; i < c; i++)
+    n1 = n; //storing the original number 
+
+    //Logic to sum the number of digits in a given number
+    while (n > 0)
     {
-        cout << no[i];
+        m  = n % 10;  // 5, 4, 3, 2, 1       // 1, 1, 1    
+        num += m;     // 5, 9, 12, 14, 15    // 1, 2, 3      
+        n = n / 10;   // 1234, 123, 12, 1    // 11, 1, 0          
     }
 
-    cout << " is " << sum;
+    cout << "The sum of digits of " << n1 << " is " << num;
 
-    return 0;    
+    return 0;
 }
